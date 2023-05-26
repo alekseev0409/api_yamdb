e10254e157da8e7d21f270348dbd05f74fc15798
Valentin Lvov <lvovvalentin1@gmail.com>
2023-05-26 20:38:49 +0300

    Маленькие внесения: reviewsviewset and commentviewset + добавление некоторых эднпоинтов

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,8 +1,18 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import TokenView, UserViewSet, SignUpView
+from .views import (TokenView, UserViewSet, SignUpView,
+                    CategoryViewSet, GenreViewSet, TitleViewSet,
+                    ReviewViewSet, CommentViewSet,
+                    UserViewSet)
 
 router = DefaultRouter()
+router.register(r'categories', CategoryViewSet)
+router.register(r'genres', GenreViewSet)
+router.register(r'titles', TitleViewSet)
+router.register(r'titles/(?P<title_id>\d+)/reviews',
+                ReviewViewSet, basename='reviews')
+router.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)'
+                r'/comments', CommentViewSet, basename='comments')
 router.register('users', UserViewSet, basename='users')
 urlpatterns = [
     path('v1/', include(router.urls)),
