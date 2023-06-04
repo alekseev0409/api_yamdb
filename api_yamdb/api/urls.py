a0eb2c9de2593650b08f858c60aa9860b531aea4
Алексей Алексеев <alekseev0409@gmail.com>
2023-06-04 21:37:04 +0300

    fix code

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -13,8 +13,13 @@ router.register(r'titles/(?P<title_id>\d+)/reviews',
 router.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)'
                 r'/comments', CommentViewSet, basename='comments')
 router.register('users', UserViewSet, basename='users')
+
+token_patterns = [
+    path('token/', TokenView.as_view(), name='token'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+]
+
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('v1/auth/token/', TokenView.as_view(), name='token'),
-    path('v1/auth/signup/', SignUpView.as_view(), name='signup'),
+    path('v1/auth/', include(token_patterns)),
 ]
